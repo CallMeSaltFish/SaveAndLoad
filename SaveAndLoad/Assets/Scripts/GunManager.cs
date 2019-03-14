bf9d16c3a3161e60e075d519239224f69fc7da7f
CallMeSaltFish <573519596@qq.com>
2019-03-14 23:00:30 +0800

    2019.3.14

---
+++ SaveAndLoad/Assets/Scripts/GunManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour
+{
+    //最大及最小的x,y轴的旋转角度
+    private float maxYRotation = 120;
+    private float minYRotation = 0;
+    private float maxXRotation = 60;
+    private float minXRotation = 0;
+
+    private float shootTime = 1;
+    private float shootTimer = 0;
+
+    public GameObject bulletGo;
+    public Transform firePosition;
+    [Range(1500, 5000)]
+    public float force;
+    private void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if(shootTimer > shootTime)
+        {
+            //TODO:可以射击
+            if(Input.GetMouseButtonDown(0))
+            {
+                GameObject bulletCurrent = GameObject.Instantiate(bulletGo, firePosition.position, Quaternion.identity);
+                Debug.Log(2);
+                bulletCurrent.GetComponent<Rigidbody>().AddForce(transform.forward * force/*, ForceMode.VelocityChange*/);
+                shootTimer = 0;
+            }
+        }
+        float xPosPercent = Input.mousePosition.x / Screen.width;
+        float yPosPercent = Input.mousePosition.y / Screen.height;
+
+        float xAngle = -Mathf.Clamp(yPosPercent * maxXRotation, minXRotation, maxXRotation) + 15;
+        float yAngle = Mathf.Clamp(xPosPercent * maxYRotation, minYRotation, maxYRotation) - 60;
+
+        transform.eulerAngles = new Vector3(xAngle, yAngle, 0);
+    }
+}
---
+++ SaveAndLoad/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+
+    public GameObject[] monsters;
+    public GameObject activeMonster = null;
+
+    private float timer = 0;
+
+    private void Start()
+    {
+        foreach(GameObject monster in monsters)
+        {
+            monster.GetComponent<BoxCollider>().enabled = false;
+            monster.SetActive(false);
+        }
+        //ActivateMonster();
+        StartCoroutine("AliveTimer");
+        //StartCoroutine("DeathTimer");
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+    }
+    private void ActivateMonster()
+    {
+        int index = Random.Range(0, monsters.Length);
+        activeMonster = monsters[index];
+        activeMonster.SetActive(true);
+        activeMonster.GetComponent<BoxCollider>().enabled = true;
+        StartCoroutine("DeathTimer");
+    }
+
+    private void DeActiveMonster()
+    {
+        if(activeMonster != null)
+        {
+            activeMonster.GetComponent<BoxCollider>().enabled = false;
+            activeMonster.SetActive(false);
+            activeMonster = null;
+        }
+        StartCoroutine("AliveTimer");
+    }
+
+
+    IEnumerator AliveTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        ActivateMonster();
+    }
+
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 8));
+        DeActiveMonster();
+    }
+}
